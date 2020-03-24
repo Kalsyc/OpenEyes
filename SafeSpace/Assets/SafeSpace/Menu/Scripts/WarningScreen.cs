44b95b9dfbeda788b69f791dd95e1b52c5988764
Darren Sim <klassiclegend@gmail.com>
2020-03-24 16:25:09 +0800

    Add Warning Screen

---
+++ SafeSpace/Assets/SafeSpace/Menu/Scripts/WarningScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WarningScreen : MonoBehaviour
+{
+    public void GoToMenu()
+    {
+        Debug.Log("Go to Menu");
+        SceneManager.LoadScene((int)SceneIndexes.MENU_SCREEN);
+    }
+
+    public void QuitApplication()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
