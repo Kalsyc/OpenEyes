c4fcdc33a95ac1c4d067635582d8599c9f8871f1
Arthur Lee <ykarthurlee@gmail.com>
2020-03-25 15:30:24 +0800

    Folder Strcuture and base code for event manager

---
+++ SafeSpace/Assets/SafeSpace/Simulation/Scripts/EventManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    //using colliders to trigger
+    // https://answers.unity.com/questions/652115/multiple-independent-event-and-one-delegate.html
+    public delegate void TriggerAction(Collider other);
+    public static event TriggerAction OnDocument;
+    public static event TriggerAction OnCalendar;
+    public static event TriggerAction OnSlides;
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Document")) {
+            if (OnDocument != null) {
+                OnDocument(other);
+            }
+        } else if (other.gameObject.CompareTag("Calendar")) {
+            if (OnCalendar != null) {
+                OnCalendar(other);
+            }
+        } else if (other.gameObject.CompareTag("Slides")) {
+            if (OnSlides != null) {
+                OnSlides(other);
+            }
+        }
+    }
+}
