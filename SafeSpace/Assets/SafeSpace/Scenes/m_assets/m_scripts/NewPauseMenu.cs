dfee85ebc0d5fb1ebe2f5ab2d12021e7a22a0b90
bendymochi <meiyen-x@hotmail.com>
2020-03-30 17:21:27 +0800

    Added Menus & Meditation Environment

---
+++ SafeSpace/Assets/SafeSpace/Scenes/m_assets/m_scripts/NewPauseMenu.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewPauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    void Start ()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            Debug.Log("Key");
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Debug.Log("Game Resumed");
+
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        // freeze game
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        Debug.Log("Game Paused");
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        Debug.Log("Loading Menu...");
+        //SceneManager.LoadScene("Menu");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+
+}
+
