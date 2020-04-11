487ae57e5b8ceb86ada5664fc05fc3cc74d31630
Darren Sim <klassiclegend@gmail.com>
2020-04-11 18:08:59 +0800

    Add async loading

---
+++ SafeSpace/Assets/ObjectLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ObjectLoader : MonoBehaviour
+{
+    public GameObject objectsToLoad;
+
+    public void Start()
+    {
+        SceneManager.sceneUnloaded += OnUnloadMenu;
+    }
+    public void OnUnloadMenu(Scene current)
+    {
+        objectsToLoad.SetActive(true);
+    }
+
+}
