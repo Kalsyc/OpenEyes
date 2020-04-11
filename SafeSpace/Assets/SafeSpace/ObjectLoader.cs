6bfead679eb0ebf5712031034a4841f034e07d00
Darren Sim <klassiclegend@gmail.com>
2020-04-12 03:13:19 +0800

    Link mode

--- SafeSpace/Assets/SafeSpace/ObjectLoader.cs
+++ SafeSpace/Assets/SafeSpace/ObjectLoader.cs
@@ -14,6 +14,7 @@ public class ObjectLoader : MonoBehaviour
     public void OnUnloadMenu(Scene current)
     {
         objectsToLoad.SetActive(true);
+        SceneManager.sceneUnloaded -= OnUnloadMenu;
     }
 
 }
