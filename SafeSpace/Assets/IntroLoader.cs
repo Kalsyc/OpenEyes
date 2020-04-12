b4c6c92d0c978f8c9e9da006c49c492745fa7d0c
Darren Sim <klassiclegend@gmail.com>
2020-04-12 18:29:22 +0800

    Link Meditation Menu to Main Menu

---
+++ SafeSpace/Assets/IntroLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroLoader : MonoBehaviour
+{
+    public GameObject introPanel;
+    public GameObject nextPanel;
+
+    public void LoadNext()
+    {
+        introPanel.SetActive(false);
+        nextPanel.SetActive(true);
+    }
+
+}
---
+++ SafeSpace/Assets/SafeSpace/Menu/Scripts/MenuManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void LoadMeditation()
+    {
+        SceneManager.LoadScene((int)SceneIndexes.MEDITATION_MENU);
+    }
+
+    public void LoadSimulation()
+    {
+        SceneManager.LoadScene((int)SceneIndexes.SIMULATION_SCREEN);
+    }
+}
