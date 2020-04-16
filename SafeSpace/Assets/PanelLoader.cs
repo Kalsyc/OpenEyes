d5b9774df7dc145973fc91832f5a9d2239b50227
bendymochi <meiyen-x@hotmail.com>
2020-04-16 14:27:45 +0800

    Changes: - Changed the tree assets in environments - Added animation for expanding circle (inhale, exhale) - Added more rocks in Grass & changed lighting

---
+++ SafeSpace/Assets/PanelLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void checkModes()
+    {
+        if(PlayerPrefs.GetInt("mode") == 0)
+        {
+            Debug.Log("Mode is 0 here.");
+        } else
+        {
+            Debug.Log("Mode is 1 here.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
