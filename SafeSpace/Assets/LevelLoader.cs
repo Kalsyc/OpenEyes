a62bb01abf547441cbb12f8f65b79cf02bcc35dc
Darren Sim <klassiclegend@gmail.com>
2020-04-19 23:39:05 +0800

    Replace all cameras with prefab

--- SafeSpace/Assets/LevelLoader.cs
+++ SafeSpace/Assets/LevelLoader.cs
@@ -7,11 +7,7 @@ public class LevelLoader : MonoBehaviour
 {
     private GameObject pointer;
     private int toLoad;
-    private AsyncOperation operation;
-    private float progress;
 
-    public Slider slider;
-    public GameObject button;
     void Start()
     {
         while (pointer == null)
@@ -20,48 +16,13 @@ public class LevelLoader : MonoBehaviour
         }
         Debug.Log(pointer);
         toLoad = (int)pointer.GetComponent<NextLevelPointer>().GetPointer();
-        StartCoroutine(LoadAsync(toLoad));
 
 
     }
 
-    void Update()
+    public void LoadLevel()
     {
-        if (progress == 1f)
-        {
-
-
-            button.SetActive(true);
-        }
-    }
-
-    IEnumerator LoadAsync(int sceneIndex)
-    {
-        operation = SceneManager.LoadSceneAsync(sceneIndex, LoadSceneMode.Additive);
-        operation.allowSceneActivation = false;
-        
-        while (!operation.isDone)
-        {
-            progress = Mathf.Clamp01(operation.progress / .9f);
-            slider.value = progress;
-            Debug.Log(progress);
-            yield return null;
-        }
-
-    }
-
-    public void EndLoadingScreen()
-    {
-        operation.allowSceneActivation = true;
-
-    }
-
-    IEnumerator SkipFrame(int i)
-    {
-        for (int j = 0; j < i; j++)
-        {
-            yield return 0;
-        }
+        SceneManager.LoadScene(toLoad);
     }
 
 }
