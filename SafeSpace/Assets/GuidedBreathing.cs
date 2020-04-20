641fd419a128f0db2ae572b9c7e26274f9ba6251
Darren Sim <klassiclegend@gmail.com>
2020-04-21 01:59:46 +0800

    Finish up guided breathing exercises

---
+++ SafeSpace/Assets/GuidedBreathing.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Events;
+using System;
+
+public class GuidedBreathing : MonoBehaviour
+{
+    public int[] stringDuration;
+    public string[] stringShown;
+    // PlayGlowingCircle -> StopGlowingCircle -> Play CircleAnim
+    public int glowingCircleStart;
+    public int glowingCircleEnd;
+    public int breathingCircleStart;
+
+    public GameObject textObject;
+    public GameObject glowingCircle;
+    public GameObject breathingCircle;
+
+    private TMP_Text text;
+    private Dictionary<int, Action> holder = new Dictionary<int, Action>();
+
+    public void Start()
+    {
+        text = textObject.GetComponent<TMP_Text>();
+        holder.Add(glowingCircleStart, PlayGlowingCircle);
+        holder.Add(glowingCircleEnd, StopGlowingCircle);
+        holder.Add(breathingCircleStart, PlayBreathingCircle);
+    }
+    public void PlayScript()
+    {
+        StartCoroutine(PlayStep(stringShown));
+    }
+
+    IEnumerator PlayStep(string[] msg)
+    {
+        for (int i = 0; i < msg.Length; i++)
+        {
+            text.text = msg[i];
+            if (holder.ContainsKey(i))
+            {
+                Debug.Log("Helpo");
+                holder[i].Invoke();
+            }
+            yield return new WaitForSeconds(stringDuration[i]);
+        }
+    }
+
+    public void PlayBreathingCircle()
+    {
+        breathingCircle.SetActive(true);
+    }    
+
+
+    public void PlayGlowingCircle()
+    {
+        glowingCircle.SetActive(true);
+    }
+
+    public void StopGlowingCircle()
+    {
+        glowingCircle.SetActive(false);
+    }
+
+}
---
+++ SafeSpace/Assets/GuidedTriggers.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuidedTriggers : MonoBehaviour
+{
+    public GameObject glowingCircle;
+
+    public void PlayGlowingCircle()
+    {
+        glowingCircle.SetActive(true);
+    }
+
+    public void StopGlowingCircle()
+    {
+        glowingCircle.SetActive(false);
+    }
+}
